38ac67d839c2257d0366df1f81b673fdf838c854
ChristianOestraich <christianoestraich@gmail.com>
2019-02-11 16:21:13 -0200

    Atualizaçao do WordPress e permissao para mecher no diretorio web

--- web/wp-content/themes/malura/index.php
+++ web/wp-content/themes/malura/index.php
@@ -3,5 +3,18 @@
 
 <h1>Bem Vindo!!</h1>
 
+<?php
+// essa validação verifica se tem post.
+if (have_posts()) {
+
+    while (have_posts()) {
+        the_post();
+?>
+        <h2><?php the_title();?></h2> <!--essa função exibi o titulo do post.-->
+        <div><?php the_content();?></div> <!-- esse função exibi a descrição do post.-->
+<?php
+    }
+}
+?>
 <?php get_footer();?>
 <!--get_footer, faz a função do include do php -->
