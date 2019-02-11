8b88cd9378d350046ebfa7805f0f65ecc95a2b02
Christian <christian@aztecweb.net>
2019-02-11 20:44:47 -0200

    Modificaçao no wp-config.php

--- web/wp-config.php
+++ web/wp-config.php
@@ -94,6 +94,7 @@ if( 'development' === getenv( 'ENV' ) ) {
 
 	// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
 	define( 'SCRIPT_DEBUG', true );
+
 }
 
 /* That's all, stop editing! Happy blogging. */
