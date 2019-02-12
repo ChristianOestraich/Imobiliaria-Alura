771e49ff2634627afb70d29f0d4a4b4c589f1551
ChristianOestraich <christianoestraich@gmail.com>
2019-02-12 16:33:33 -0200

    Finalizilando a segunda aula do curso do alura

--- web/wp-content/themes/malura/header.php
+++ web/wp-content/themes/malura/header.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <title>Imobiliária Malura</title>
+    <link rel="stylesheet" href="<?= get_template_directory_uri();?>/assets/css/reset.css">
+    <link rel="stylesheet" href="<?= get_template_directory_uri();?>/style.css"> <!-- Esse comando traz a o style.css de dentro do tema-->
     <?php wp_head()?> <!--Coloca o painel de administrador no cabeçario do site -->
 </head>
 <body>
--- web/wp-content/themes/malura/index.php
+++ web/wp-content/themes/malura/index.php
@@ -1,21 +1,29 @@
 <!--get_header, faz a função do include do php, ele é padrão do WordPress -->
 <?php get_header();?>
 
-<h1>Bem Vindo!!</h1>
+<main class="home-main">
+    <div class="container">
 
-<?php
-// essa validação verifica se tem post.
-if (have_posts()) {
+        <h1>Bem Vindo ao Maluras!</h1>
+        <ul class="imoveis-listagem">
+            <?php
+                // essa validação verifica se tem post.
+                if (have_posts()) {
 
-    while (have_posts()) {
-        the_post();
-?>
-        <?php the_post_thumbnail();?> <!-- essa função exibi a imagem em destaque. -->
-        <h2><?php the_title();?></h2> <!-- essa função exibi o titulo do post.-->
-        <div><?php the_content();?></div> <!-- esse função exibi a descrição do post.-->
-<?php
-    }
-}
-?>
+                    while (have_posts()) {
+                        the_post();
+                ?>
+                        <li class="imoveis-listagem-item">
+                            <?php the_post_thumbnail();?> <!-- essa função exibi a imagem em destaque. -->
+                            <h2><?php the_title();?></h2> <!-- essa função exibi o titulo do post.-->
+                            <div><?php the_content();?></div> <!-- esse função exibi a descrição do post.-->
+                        </li>
+                <?php
+                    }
+                }
+            ?>
+        </ul>
+    </div>
+</main>
 <?php get_footer();?>
 <!--get_footer, faz a função do include do php -->
