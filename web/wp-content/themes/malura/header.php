544dedcfad8b1d62d3bae251a5c41c7c84516c4d
ChristianOestraich <christianoestraich@gmail.com>
2019-02-13 08:04:22 -0200

    Foi colocado um comentario na chamada do reset.css

--- web/wp-content/themes/malura/header.php
+++ web/wp-content/themes/malura/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Imobiliária Malura</title>
-    <link rel="stylesheet" href="<?= get_template_directory_uri();?>/assets/css/reset.css">
+    <link rel="stylesheet" href="<?= get_template_directory_uri();?>/assets/css/reset.css"> <!-- Esse comando traz a o reset.css de dentro do tema-->
     <link rel="stylesheet" href="<?= get_template_directory_uri();?>/style.css"> <!-- Esse comando traz a o style.css de dentro do tema-->
     <?php wp_head()?> <!--Coloca o painel de administrador no cabeçario do site -->
 </head>
