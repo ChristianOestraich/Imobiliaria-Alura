07ea3d635c233cde36a7bcdb69331b696734c388
ChristianOestraich <christianoestraich@gmail.com>
2019-02-13 09:33:47 -0200

    Foi desenvolvido alguns arrays para o register_post_type

--- web/wp-content/themes/malura/functions.php
+++ web/wp-content/themes/malura/functions.php
@@ -1,3 +1,17 @@
 <?php
 
 add_theme_support('post-thumbnails'); // Ele adiciona ao post a imagem destacada.
+
+$labels = array(
+
+    'name' => 'Imóveis',
+    'name_singular' => 'Imóvel'
+);
+
+$args = array( //  Aqui aonde é feita a configuração
+
+    'labels'=> $labels, // aqui vai ser passado o nome do novo conteudo, dentro de um array de nomes.
+    'public'=> true // para deixar ele publico no painel do WordPress.
+);
+
+register_post_type('imovel', $args ); // Esse comando registra um novo conteudo no painel.Primeiro passo é passar como parametro o apelido e o segundo é uma configuração.
