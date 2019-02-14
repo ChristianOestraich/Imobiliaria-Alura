7da04b53bc712aea932248a9b361822c305d378d
ChristianOestraich <christianoestraich@gmail.com>
2019-02-14 11:09:44 -0200

    Foi adicionado um formulario

--- web/wp-content/themes/malura/page.php
+++ web/wp-content/themes/malura/page.php
@@ -20,8 +20,27 @@
        <?php
             }
         }
-        ?>
+            if( is_page('contato') ) {
+            ?>
+            <form>
+			<div class="form-nome">
+				<label for="form-nome">Nome:</label>
+				<input id="form-nome" type="text" placeholder="Seu nome aqui" name="form-nome">
+			</div>
 
+			<div class="form-email">
+				<label for="form-email">Email:</label>
+				<input id="form-email" type="email" placeholder="email@exemplo.com.br" name="form-email">
+			</div>
+
+			<div class="form-mensagem">
+				<label for="form-mensagem">Mensagem:</label>
+				<textarea id="form-mensagem" name="form-mensagem"></textarea>
+			</div>
+			<button type="submit">Enviar</button>
+
+		</form>
+    <?php } ?>
     </article>
 </main>
 
