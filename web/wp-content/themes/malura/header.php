3dea758f41651d3e95321574a6bec625f9f6fa31
ChristianOestraich <christianoestraich@gmail.com>
2019-02-14 15:38:40 -0200

    Foi adionado o comando bloginfo

--- web/wp-content/themes/malura/header.php
+++ web/wp-content/themes/malura/header.php
@@ -3,13 +3,17 @@
 <head>
 	<?php $home = get_template_directory_uri(); ?>
     <meta charset="utf-8">
-    <title>Imobiliária Malura</title>
 	<link rel="stylesheet" href="<?= $home ?>/assets/css/reset.css"> <!-- Esse comando traz a o reset.css de dentro do tema-->
 	<link rel="stylesheet" href="<?= $home; ?>/assets/css/index.css"> <!-- Esse comando traz a o index.css de dentro do tema-->
     <link rel="stylesheet" href="<?= $home; ?>/assets/css/comum.css"> <!-- Esse comando traz a o comum.css de dentro do tema-->
     <link rel="stylesheet" href="<?= $home; ?>/assets/css/header.css"> <!-- Esse comando traz a o header.css de dentro do tema-->
     <link rel="stylesheet" href="<?= $home; ?>/assets/css/page.css"> <!-- Esse comando traz a o page.css de dentro do tema-->
     <link rel="stylesheet" href="<?= $home; ?>/assets/css/single.css"> <!-- Esse comando traz a o single.css de dentro do tema-->
+
+    <!-- deixa a criterio do usuário modificar o nome do site no painel do WordPress. -->
+    <title>
+        <?php bloginfo('name')?>
+    </title>
 
 	<?php wp_head(); ?> <!--Coloca o painel de administrador no cabeçario do site -->
 </head>
