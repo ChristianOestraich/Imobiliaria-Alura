7ca118da49a597e4c0de80d7f2e9213a840d2675
ChristianOestraich <christianoestraich@gmail.com>
2019-02-15 15:27:11 -0200

    Finalizado o projeto do alura

--- web/wp-content/themes/malura/single.php
+++ web/wp-content/themes/malura/single.php
@@ -24,6 +24,19 @@
 					<?php the_content(); ?>
 				</div>
 
+                <?php $imoveis_meta_data = get_post_meta( $post->ID );?>
+
+                <dl class="single-imovel-informacoes">
+
+                    <dt>Preço:</dt>
+                    <dd><?= $imoveis_meta_data['preco_id'][0] ?></dd>
+                    <dt>Vagas:</dt>
+                    <dd><?= $imoveis_meta_data['vagas_id'][0] ?></dd>
+                    <dt>Preço:</dt>
+                    <dd><?= $imoveis_meta_data['banheiros_id'][0] ?></dd>
+                    <dt>Preço:</dt>
+                    <dd><?= $imoveis_meta_data['quartos_id'][0] ?></dd>
+                </dl>
 
 			</section>
 
